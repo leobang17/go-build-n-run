e44cca76f85c70a0a1f026ee5aa0b4e432687004
LeoBang <bluke8489@gmail.com>
2024-04-02 16:22:33 +0900

    go init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leobang17/go-build-n-run
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Standalone Go program for build and run short cuts.")
+}
